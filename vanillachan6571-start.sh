06f57f5c6f16ec27c050c86ce5e7ba16d8cae80e
VanillaChanny <39040261+VanillaChan6571@users.noreply.github.com>
2023-06-11 16:24:40 -0600

    Update vanillachan6571-start.sh
    Changed Xmx value to 1024 (1GB) to attempt to fix out of memory when left running for 14+ days. ( You can always edit after install in vanillachan6571-start.sh )

--- vanillachan6571-start.sh
+++ vanillachan6571-start.sh
@@ -6,9 +6,9 @@ if [ -x "$(command -v java)" ]; then
   # if screen is present use that
   # this check is elevated as tmux is sometimes present by default
   if [ -x "$(command -v screen)" ]; then
-    screen -DmS CloudNet java -Xms256M -Xmx256M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar nogui
+    screen -DmS CloudNet java -Xms256M -Xmx1024M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar nogui
   elif [ -x "$(command -v tmux)" ]; then
-    tmux new-session -As CloudNet java -Xms256M -Xmx256M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar nogui
+    tmux new-session -As CloudNet java -Xms256M -Xmx1024M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar nogui
   else
     echo "No screen or tmux installation found, you need to install at least one of them to run CloudNet"
     exit 1
